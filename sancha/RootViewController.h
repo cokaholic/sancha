dea1703bc552147c2ba8b364516b9de80ffe22a1
Keisuke Tatsumi <nietzsche.god.is.dead@gmail.com>
2015-07-04 11:55:23 +0900

    added RootViewController

---
+++ sancha/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  sancha
+//
+//  Created by Keisuke_Tatsumi on 2015/07/04.
+//  Copyright (c) 2015年 Keisuke Tatsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
